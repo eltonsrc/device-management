b052317437786442985d7c2ced739f285080dc66
Elton Serra <eltonsrc@gmail.com>
2022-03-14 23:18:46 -0400

    Add modules for device and category
    Signed-off-by: Elton Serra <eltonsrc@gmail.com>

--- device-mng-backend/src/app.module.ts
+++ device-mng-backend/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryModule } from './category/category.module';
+import { Category } from './category/category.entity';
+import { DeviceModule } from './device/device.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'device-mng',
+      entities: [Category],
+      synchronize: true,
+    }),
+    CategoryModule,
+    DeviceModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
